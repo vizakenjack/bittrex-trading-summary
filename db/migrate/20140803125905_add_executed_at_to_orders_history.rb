ef36bed5b2daab852c1384161cd22cc2040c15f3
Vizakenjack <vizakenjack@gmail.com>
2014-08-09 19:35:48 +0400

    OrderHistory migration to update executed_at.

--- db/migrate/20140803125905_add_executed_at_to_orders_history.rb
+++ db/migrate/20140803125905_add_executed_at_to_orders_history.rb
@@ -1,6 +1,6 @@
 class AddExecutedAtToOrdersHistory < ActiveRecord::Migration
   def change
     add_column :orders_histories, :executed_at, :datetime
+    OrdersHistory.update_all "executed_at = created_at"
   end
-  OrdersHistory.update_all "executed_at = created_at"
 end
